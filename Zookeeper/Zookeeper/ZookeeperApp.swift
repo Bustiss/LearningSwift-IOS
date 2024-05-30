f0081d7b6fa5315e94a087a9825681951622d441
Willie Jones <90412259+Bustiss@users.noreply.github.com>
2024-05-29 21:19:56 -0500

    Zookeeper
    Added zookeeper for List and Section practice

---
+++ Zookeeper/Zookeeper/ZookeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeperApp.swift
+//  Zookeeper
+//
+//  Created by Willie Jones on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
